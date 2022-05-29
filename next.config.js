d81c31266239109206becb9cce641a671cef5d7c
Rafik Gajiyev <rafik.gadzhiyev.03@bk.ru>
2022-05-29 20:04:00 +0600

    image optimization problem

--- next.config.js
+++ next.config.js
@@ -5,7 +5,9 @@ const nextConfig = {
     styledComponents: true
   },
   images: {
-    domains: ["cdn2.thecatapi.com", "24.media.tumblr.com", "28.media.tumblr.com", "27.media.tumblr.com", "26.media.tumblr.com", "25.media.tumblr.com"]
+    domains: ["cdn2.thecatapi.com", "24.media.tumblr.com", "28.media.tumblr.com", "27.media.tumblr.com", "26.media.tumblr.com", "25.media.tumblr.com"],
+    loader: 'imgix',
+    path: '/'
   }
 }
 
